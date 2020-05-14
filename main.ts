ef0dcc6e7c03b58400a4e442c3bd7e5dd218a5de
Dung Truong <checkraiser11@gmail.com>
2020-05-14 17:55:11 +0700

    Add hash

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 import { serve } from "https://deno.land/std@v0.50.0/http/server.ts";
 import * as flags from "https://deno.land/std@v0.50.0/flags/mod.ts";
-
+import { Sha256 } from "https://deno.land/std@v0.50.0/hash/sha256.ts";
+function toHexString(value: number[] | ArrayBuffer): string {
+  const array = new Uint8Array(value);
+  let hex = "";
+  for (const v of array) {
+    const c = v.toString(16);
+    hex += c.length === 1 ? `0${c}` : c;
+  }
+  return hex;
+}
+const algorithm = new Sha256()
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
@@ -9,8 +19,9 @@ if (isNaN(port)) {
   console.error("Port is not a number.");
   Deno.exit(1);
 }
-
-const body = new TextEncoder().encode("Hello World\n");
+algorithm.update("Hello world");
+const test = algorithm.hex();
+const body = new TextEncoder().encode(test);
 const s = serve({ port: port, hostname: '0.0.0.0'});
 console.log("http://localhost:" + port);
 
